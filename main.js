c42b301413354ff40d177397edb51ace83c5eb3e
Shiviara <113165919+Shiviara@users.noreply.github.com>
2025-03-29 20:02:02 +0100

    base

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 import * as THREE from "three";
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGL3DRender({
+  canvas: document.querySelector("#bg"),
+});
+
+renderer.setPixelRation(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene, camera);
